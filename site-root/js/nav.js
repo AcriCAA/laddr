447c17ee83a6d9af39fe7b7948ee791056b429fd
Corey Acri <acri.corey@gmail.com>
2015-11-01 11:30:28 -0500

    removing application of class to nav tag

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -5,12 +5,12 @@ console.log("nav js loaded");
 
 $(window).scroll(function() {
   if ($(document).scrollTop() > 35) {
-    $('nav').addClass('shrink');
+    $('.navbar').addClass('shrink');
    // $('img#nav-logo').animate({width: "15px"},50,function(){});
 $('img#nav-logo').css('width', '15px');
    
   } else if($(document).scrollTop() < 35){
-    $('nav').removeClass('shrink');
+    $('.navbar').removeClass('shrink');
    //$('img#nav-logo').animate({width: "100px"}, 50, function(){});
    
    $('img#nav-logo').css('width', '100px');
