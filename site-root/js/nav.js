c560d513dd81f1dd5604501f7c75c7fb2683d9fe
Corey Acri <acri.corey@gmail.com>
2015-11-01 10:30:31 -0500

    fixing resize timing

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -6,11 +6,11 @@ console.log("nav js loaded");
 $(window).scroll(function() {
   if ($(document).scrollTop() > 35) {
     $('nav').addClass('shrink');
-    $('img#nav-logo').animate({width: "3%","padding-top":"3px"},300,function(){});
+    $('img#nav-logo').animate({width: "10px","padding-top":"3px"},300,function(){});
 
   } else {
     $('nav').removeClass('shrink');
-   $('img#nav-logo').animate({width: "5%"},300,function(){});
+   $('img#nav-logo').animate({width: "58px"},300,function(){});
      
   }
 });
