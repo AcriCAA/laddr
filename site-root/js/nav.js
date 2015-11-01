d91f2e7a34ce6788e964363f17d5f7d5e3eabf42
Corey Acri <acri.corey@gmail.com>
2015-11-01 10:42:25 -0500

    resizing on scroll up or down

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -4,18 +4,16 @@ console.log("nav js loaded");
 
 
 $(window).scroll(function() {
-  if ($(document).window.pageYOffset > 35) {
+  if ($(document).scrollTop() > 35) {
     $('nav').addClass('shrink');
     $('img#nav-logo').animate({width: "10px","padding-top":"3px"},300,function(){});
 
-  } else if ($(document).window.pageYOffset == 0) {
+  } else ($(document).scrollTop() < 35){
     $('nav').removeClass('shrink');
-   $('img#nav-logo').animate({width: "58px"},300,function(){});
+   $('img#nav-logo').animate({width: "59px"},300,function(){});
      
   }
 });
 
 
    
-   //if(window.pageYOffset > 35 && !collapsedBar)
-   	//else if(window.pageYOffset == 0)
