8d18b3e56d45b62dc233cc2d4f4f7049f73e7c6a
Corey Acri <acri.corey@gmail.com>
2015-11-01 10:23:30 -0500

    resizing logo

--- html-templates/includes/site.brand.tpl
+++ html-templates/includes/site.brand.tpl
@@ -1,1 +1,1 @@
-<img class="nav-logo" alt="{Laddr::$siteName|escape}" src="{versioned_url img/logo.png}"><a class="navbar-brand" href="/"></a>
+<img id="nav-logo" alt="{Laddr::$siteName|escape}" src="{versioned_url img/logo.png}"><a class="navbar-brand" href="/"></a>
--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -5,11 +5,15 @@ console.log("nav js loaded");
 
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
-     $('nav-logo').css('width', '3%');
+    $('nav').animate(.addClass('shrink'));
+    $('img#nav-logo').animate({width: "3%","padding-top":"3px"},300,function(){});
 
   } else {
-    $('nav').removeClass('shrink');
+    $('nav').animate(.removeClass('shrink'));
+   $('img#nav-logo').animate({width: "5%"},300,function(){});
      
   }
 });
+
+
+   
