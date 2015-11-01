49d21acb7556be3a5c6a3d85382967cce5c64996
Corey Acri <acri.corey@gmail.com>
2015-11-01 10:18:19 -0500

    resizing logo

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -6,8 +6,7 @@ console.log("nav js loaded");
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
     $('nav').addClass('shrink');
-     $('nav-logo').css('height', '1%');
-     $('nav-logo').css('width', '1%');
+     $('nav-logo').css('width', '3%');
 
   } else {
     $('nav').removeClass('shrink');
