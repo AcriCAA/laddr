4158b7af7d94c4e8972d67171c542d5629c984b5
Corey Acri <acri.corey@gmail.com>
2015-11-01 11:23:27 -0500

    modifying logo size

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -6,11 +6,11 @@ console.log("nav js loaded");
 $(window).scroll(function() {
   if ($(document).scrollTop() > 35) {
     $('nav').addClass('shrink');
-    $('img#nav-logo').animate({width: "15px","padding-top":"2px"},50,function(){});
+    $('img#nav-logo').animate({width: "15px"},50,function(){});
 
   } else if($(document).scrollTop() < 35){
     $('nav').removeClass('shrink');
-   $('img#nav-logo').animate({width: "59px"}, 50, function(){});
+   $('img#nav-logo').animate({width: "100px"}, 50, function(){});
      
   }
 });
