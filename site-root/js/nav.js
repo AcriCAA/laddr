c2575da83092ae4759e37d67c46071b5ff3f86bc
Corey Acri <acri.corey@gmail.com>
2015-10-31 23:47:25 -0400

    rewriting js to shrink nav and nav logo

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -2,22 +2,16 @@
 
 console.log("nav js loaded");
 
-if(window.pageYOffset > 35 && !collapsedBar)
 
-{  // collapsed size
-    collapsedBar = true;
-    //collapse bar
-   
-    $('.nav-logo').css('height', '100%');
-     $('.nav-logo').css('width', '100%');
-   
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+     $('nav-logo').css('height', '25%');
+     $('nav-logo').css('width', '25%');
 
-
-  }else if(window.pageYOffset == 0){ //full size
-    collapsedBar = false;
-    $('.nav-logo').css('height', '50%');
-     $('.nav-logo').css('width', '50%');
-   
-   
-   
+  } else {
+    $('nav').removeClass('shrink');
+     $('nav-logo').css('height', '50%');
+     $('nav-logo').css('width', '50%');
   }
+});
