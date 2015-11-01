8d4199a902894c2d16ceab74e24c2e2ee18587f9
Corey Acri <acri.corey@gmail.com>
2015-11-01 10:24:53 -0500

    resizing logo

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -5,11 +5,11 @@ console.log("nav js loaded");
 
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
-    $('nav').animate(.addClass('shrink'));
+    $('nav').addClass('shrink');
     $('img#nav-logo').animate({width: "3%","padding-top":"3px"},300,function(){});
 
   } else {
-    $('nav').animate(.removeClass('shrink'));
+    $('nav').removeClass('shrink');
    $('img#nav-logo').animate({width: "5%"},300,function(){});
      
   }
