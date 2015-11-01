8248b67c031a98618796cc493d77c7a1c2e14878
Corey Acri <acri.corey@gmail.com>
2015-10-31 23:26:04 -0400

    adding js

--- html-templates/includes/site.js-bottom.tpl
+++ html-templates/includes/site.js-bottom.tpl
@@ -12,4 +12,5 @@
     ,'bootstrap/bootstrap-tooltip.js'
     ,'bootstrap-combobox.js'
     ,'bootstrap-tagsinput.js'
+    ,'nav.js'
 )}
--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -4,8 +4,8 @@ if(window.pageYOffset > 35 && !collapsedBar){  // collapsed size
     collapsedBar = true;
     //collapse bar
    
-    $('.navbar-brand').css('height', '50%');
-     $('.navbar-brand').css('width', '50%');
+    $('.brand').css('height', '50%');
+     $('.brand').css('width', '50%');
    
    
    
@@ -14,8 +14,8 @@ if(window.pageYOffset > 35 && !collapsedBar){  // collapsed size
 
   }else if(window.pageYOffset == 0){ //full size
     collapsedBar = false;
-    $('.navbar-brand').css('height', '100%');
-     $('.navbar-brand').css('width', '100%');
+    $('.brand').css('height', '100%');
+     $('.brand').css('width', '100%');
    
    
    
