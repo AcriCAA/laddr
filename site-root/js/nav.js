bc6280fa2d7419442f59b293f81f1eaeeb7452ec
AcriCAA <acri.corey@gmail.com>
2015-11-03 18:45:36 -0500

    trying to animate

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -7,13 +7,13 @@ $(window).scroll(function() {
   if ($(document).scrollTop() > 35) {
     $('.navbar').addClass('shrink');
    // $('img#nav-logo').animate({width: "15px"},50,function(){});
-$('img#nav-logo').animate({'width', '15px'}, 5000, "linear");
+$('img#nav-logo').animate({"width": "20px"}, 5000, "linear");
    
   } else if($(document).scrollTop() < 35){
     $('.navbar').removeClass('shrink');
    //$('img#nav-logo').animate({width: "100px"}, 50, function(){});
    
-   $('img#nav-logo').animate({'width', '100px'}, 5000, "linear");
+   $('img#nav-logo').animate({"width": "100px"}, 5000, "linear")	;
        
   }
 });
