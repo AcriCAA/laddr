40dec74d8b79e55e34eb9c0171aab44758fe9dce
AcriCAA <acri.corey@gmail.com>
2015-11-03 18:48:42 -0500

    trying to animate

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -6,12 +6,12 @@ console.log("nav js loaded");
 $(window).scroll(function() {
   if ($(document).scrollTop() > 35) {
     $('.navbar').addClass('shrink');
-   $('img#nav-logo').animate({width: "15px"},fast, function(){});
+   $('img#nav-logo').animate({width: "15px"}, 200, function(){});
 //$('img#nav-logo').animate({"width": "20px"}, 5000, "linear");
    
   } else if($(document).scrollTop() < 35){
     $('.navbar').removeClass('shrink');
-   $('img#nav-logo').animate({width: "100px"}, fast, function(){});
+   $('img#nav-logo').animate({width: "100px"}, 200, function(){});
    
   // $('img#nav-logo').animate({"width": "100px"}, 5000, "linear");
        
