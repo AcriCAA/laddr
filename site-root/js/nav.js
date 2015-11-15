1f4d20b83da93baa19b05be054c638bd122e2436
AcriCAA <acri.corey@gmail.com>
2015-11-14 23:38:48 -0500

    fixing the logo shrink expand

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -3,22 +3,47 @@
 console.log("nav js loaded");
 
 
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 35) {
-    $('.navbar').addClass('shrink');
-   $('img#nav-logo').animate({width: "15px"}, 200, function(){});
-   console.log("ran shrink");
-//$('img#nav-logo').animate({"width": "20px"}, 5000, "linear");
+// $(window).scroll(function() {
+//   if ($(document).scrollTop() > 50) {
+//     $('.navbar').addClass('shrink');
+//    $('img#nav-logo').animate({width: "15px"}, 200, function(){});
+//    console.log("ran shrink");
+// //$('img#nav-logo').animate({"width": "20px"}, 5000, "linear");
    
-  } else if($(document).scrollTop() < 50){
-    $('.navbar').removeClass('shrink');
-   $('img#nav-logo').animate({width: "100px"}, 200, function(){});
-   console.log("ran expand");
+//   } else if($(document).scrollTop() < 50){
+//     $('.navbar').removeClass('shrink');
+//    $('img#nav-logo').animate({width: "100px"}, 200, function(){});
+//    console.log("ran expand");
    
-  // $('img#nav-logo').animate({"width": "100px"}, 5000, "linear");
+//   // $('img#nav-logo').animate({"width": "100px"}, 5000, "linear");
        
-  }
-});
+//   }
+// });
 
+
+$(document).ready(function(){
+
+
+var collapsedBar = false; 
+
+ if(window.pageYOffset > 35 && !collapsedBar){  // collapsed size
+          collapsedBar = true;
+          //collapse bar
+         
+          $('img#nav-logo').animate({width: "20px"},300,function(){});
+          
+         // document.getElementById('span1').innerHTML='some value...';
+  
+
+        }else if(window.pageYOffset == 0){ //full size
+          collapsedBar = false;
+         
+         $('img#nav-logo').animate({width: "100px"},300,function(){});
+
+        }
+      
+    }
+
+}
 
    
