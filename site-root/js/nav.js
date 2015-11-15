c1ac4053cf45081b95635d78858ba1ee945c0fe8
AcriCAA <acri.corey@gmail.com>
2015-11-15 00:09:52 -0500

    changing

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -6,13 +6,13 @@ console.log("nav js loaded");
 $(window).scroll(function() {
   if ($(document).scrollTop() > 35) {
     // $('.navbar').addClass('shrink');
-   $('img#nav-logo').animate({width: "15px"}, 200, function(){});
+   $('img#nav-logo').animate({width: "15px"}, 1, function(){});
    console.log("ran shrink");
 //$('img#nav-logo').animate({"width": "20px"}, 5000, "linear");
    
   } else if($(document).scrollTop() == 0){
     // $('.navbar').removeClass('shrink');
-   $('img#nav-logo').animate({width: "100px"}, 200, function(){});
+   $('img#nav-logo').animate({width: "100px"}, 2, function(){});
    console.log("ran expand");
    
   // $('img#nav-logo').animate({"width": "100px"}, 5000, "linear");
