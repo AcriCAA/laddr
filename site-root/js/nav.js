2796fe491afb31c3b0af9a843b1af6ec1a1fdaed
AcriCAA <acri.corey@gmail.com>
2015-11-15 00:04:30 -0500

    changing

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -3,50 +3,50 @@
 console.log("nav js loaded");
 
 
-// $(window).scroll(function() {
-//   if ($(document).scrollTop() > 50) {
-//     $('.navbar').addClass('shrink');
-//    $('img#nav-logo').animate({width: "15px"}, 200, function(){});
-//    console.log("ran shrink");
-// //$('img#nav-logo').animate({"width": "20px"}, 5000, "linear");
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 35) {
+    // $('.navbar').addClass('shrink');
+   $('img#nav-logo').animate({width: "15px"}, 200, function(){});
+   console.log("ran shrink");
+//$('img#nav-logo').animate({"width": "20px"}, 5000, "linear");
    
-//   } else if($(document).scrollTop() < 50){
-//     $('.navbar').removeClass('shrink');
-//    $('img#nav-logo').animate({width: "100px"}, 200, function(){});
-//    console.log("ran expand");
+  } else if($(document).scrollTop() == 0){
+    // $('.navbar').removeClass('shrink');
+   $('img#nav-logo').animate({width: "100px"}, 200, function(){});
+   console.log("ran expand");
    
-//   // $('img#nav-logo').animate({"width": "100px"}, 5000, "linear");
+  // $('img#nav-logo').animate({"width": "100px"}, 5000, "linear");
        
-//   }
-// });
+  }
+});
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
-//collapsedBar = false; 
+// //collapsedBar = false; 
 
- if(window.pageYOffset > 20 ){  // collapsed size
-  //        collapsedBar = true;
-          //collapse bar
+//  if(window.pageYOffset > 20 ){  // collapsed size
+//   //        collapsedBar = true;
+//           //collapse bar
          
-          $('img#nav-logo').animate({width: "20px"},300,function(){});
+//           $('img#nav-logo').animate({width: "20px"},300,function(){});
           
-         // document.getElementById('span1').innerHTML='some value...';
+//          // document.getElementById('span1').innerHTML='some value...';
   
-console.log("greater than ");
-        }
+// console.log("greater than ");
+//         }
 
-        else if(window.pageYOffset == 0){ //full size
-    //      collapsedBar = false;
+//         else if(window.pageYOffset == 0){ //full size
+//     //      collapsedBar = false;
          
-         $('img#nav-logo').animate({width: "100px"},300,function(){});
+//          $('img#nav-logo').animate({width: "100px"},300,function(){});
 
-         console.log("zero");
+//          console.log("zero");
 
-        }
+//         }
       
-}); 
+// }); 
 
 
 // just changed again
