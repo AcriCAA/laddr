6f2a749c1e55439fc5bfd1ddb756f607d8971212
Corey Acri <acri.corey@gmail.com>
2015-11-15 16:55:46 -0500

    changing nav.js again

--- site-root/js/nav.js
+++ site-root/js/nav.js
@@ -1,54 +1,52 @@
 
 
-console.log("nav js loaded");
+// console.log("nav js loaded");
 
 
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 35) {
-    // $('.navbar').addClass('shrink');
-   $('img#nav-logo').animate({width: "15px"}, 300);
-   console.log("ran shrink");
-//$('img#nav-logo').animate({"width": "20px"}, 5000, "linear");
+// $(window).scroll(function() {
+//   if ($(document).scrollTop() > 35) {
+//     // $('.navbar').addClass('shrink');
+//    $('img#nav-logo').animate({width: "15px"}, 300);
+//    console.log("ran shrink");
+// //$('img#nav-logo').animate({"width": "20px"}, 5000, "linear");
    
-  } else if($(document).scrollTop() < 10){
-    // $('.navbar').removeClass('shrink');
-   $('img#nav-logo').animate({width: "100px"}, 300);
-   console.log("ran expand");
+//   } else if($(document).scrollTop() < 10){
+//     // $('.navbar').removeClass('shrink');
+//    $('img#nav-logo').animate({width: "100px"}, 300);
+//    console.log("ran expand");
    
-  // $('img#nav-logo').animate({"width": "100px"}, 5000, "linear");
+//   // $('img#nav-logo').animate({"width": "100px"}, 5000, "linear");
        
-  }
-});
+//   }
+// });
 
 
-// $(document).ready(function(){
+$(document).ready(function(){
 
+collapsedBar = false; 
 
-// //collapsedBar = false; 
-
-//  if(window.pageYOffset > 20 ){  // collapsed size
-//   //        collapsedBar = true;
-//           //collapse bar
+ if(window.pageYOffset > 35 ){  // collapsed size
+         collapsedBar = true;
+          //collapse bar
          
-//           $('img#nav-logo').animate({width: "20px"},300,function(){});
+          $('img#nav-logo').animate({width: "20px"},300,function(){});
           
-//          // document.getElementById('span1').innerHTML='some value...';
+         // document.getElementById('span1').innerHTML='some value...';
   
-// console.log("greater than ");
-//         }
+console.log("greater than ");
+        }
 
-//         else if(window.pageYOffset == 0){ //full size
-//     //      collapsedBar = false;
+        else if(window.pageYOffset == 0){ //full size
+        collapsedBar = false;
          
-//          $('img#nav-logo').animate({width: "100px"},300,function(){});
+         $('img#nav-logo').animate({width: "100px"},300,function(){});
 
-//          console.log("zero");
+         console.log("zero");
 
-//         }
+        }
       
-// }); 
+}); 
 
 
-// just changed again
 
    
